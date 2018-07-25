026ca61101d9aeb3972e3aade12518cddaecce89
rohannad <nadkarnirp@gmail.com>
2018-07-25 12:39:52 +0530

    On branch master

--- src/com/restapi/pojo/CUSTOMERS.java
+++ src/com/restapi/pojo/CUSTOMERS.java
@@ -1,9 +1,11 @@
-
 package com.restapi.pojo;
 
 import java.util.List;
 
 import javax.persistence.*;
+import javax.persistence.Table;
+
+import com.restapi.pojo.CUSTOMERADDRESS;
 
 @Entity
 @Table(name = "CUSTOMERS")
@@ -22,11 +24,9 @@ public class CUSTOMERS {
 	private List<CUSTOMERADDRESS> ADDRESS;
 	private List<ORDERS> ORDERS;
 	
-	
 	public CUSTOMERS() {
 		// TODO Auto-generated constructor stub
 	}
-
 
 	public CUSTOMERS(int cUSTID, String eMAIL, String pASSWORD, String fIRSTNAME, String lASTNAME, long pHONE,
 			List<CUSTOMERADDRESS> aDDRESS, List<com.restapi.pojo.ORDERS> oRDERS) {
@@ -41,81 +41,65 @@ public class CUSTOMERS {
 		ORDERS = oRDERS;
 	}
 
-
 	public int getCUSTID() {
 		return CUSTID;
 	}
-
 
 	public void setCUSTID(int cUSTID) {
 		CUSTID = cUSTID;
 	}
 
-
 	public String getEMAIL() {
 		return EMAIL;
 	}
-
 
 	public void setEMAIL(String eMAIL) {
 		EMAIL = eMAIL;
 	}
 
-
 	public String getPASSWORD() {
 		return PASSWORD;
 	}
-
 
 	public void setPASSWORD(String pASSWORD) {
 		PASSWORD = pASSWORD;
 	}
 
-
 	public String getFIRSTNAME() {
 		return FIRSTNAME;
 	}
-
 
 	public void setFIRSTNAME(String fIRSTNAME) {
 		FIRSTNAME = fIRSTNAME;
 	}
 
-
 	public String getLASTNAME() {
 		return LASTNAME;
 	}
-
 
 	public void setLASTNAME(String lASTNAME) {
 		LASTNAME = lASTNAME;
 	}
 
-
 	public long getPHONE() {
 		return PHONE;
 	}
-
 
 	public void setPHONE(long pHONE) {
 		PHONE = pHONE;
 	}
 
-
 	public List<CUSTOMERADDRESS> getADDRESS() {
 		return ADDRESS;
 	}
-
 
 	public void setADDRESS(List<CUSTOMERADDRESS> aDDRESS) {
 		ADDRESS = aDDRESS;
 	}
 
-
 	public List<ORDERS> getORDERS() {
 		return ORDERS;
 	}
-
 
 	public void setORDERS(List<ORDERS> oRDERS) {
 		ORDERS = oRDERS;
--- src/com/restapi/pojo/ORDERS.java
+++ src/com/restapi/pojo/ORDERS.java
@@ -1,4 +1,3 @@
-
 package com.restapi.pojo;
 
 import java.util.Date;
